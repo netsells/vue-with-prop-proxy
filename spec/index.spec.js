a7a5a1a18732c320fe7407c85d971e3e2a08d776
Sam Boylett <sam.boylett@netsells.co.uk>
2019-03-25 13:54:15 +0000

    Add tests for proxy creation

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,5 +1,39 @@
-describe('index', () => {
-    it('works', () => {
-        expect(true).toBe(true);
+import withPropProxy from '../src/index';
+
+describe('withPropProxy', () => {
+    const commonProxy = {
+        get: expect.any(Function),
+        set: expect.any(Function),
+    };
+
+    describe('with no props', () => {
+        it('adds no proxies', () => {
+            expect(withPropProxy([]).computed).toEqual({});
+        });
+    });
+
+    describe('with one prop as string', () => {
+        it('adds a proxy', () => {
+            expect(withPropProxy('foo').computed).toEqual({
+                fooProxy: commonProxy,
+            });
+        });
+    });
+
+    describe('with one prop as an array', () => {
+        it('adds a proxy', () => {
+            expect(withPropProxy(['bar']).computed).toEqual({
+                barProxy: commonProxy,
+            });
+        });
+    });
+
+    describe('with two props as an array', () => {
+        it('adds a proxy', () => {
+            expect(withPropProxy(['foo', 'bar']).computed).toEqual({
+                fooProxy: commonProxy,
+                barProxy: commonProxy,
+            });
+        });
     });
 });
